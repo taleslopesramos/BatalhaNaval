4d42df50c6fbc17de61a6a9c78b0b67c06ccd1b4
taleslopesramos <taleslopesramos@gmail.com>
2016-03-28 00:08:53 -0300

    Added files via upload
    Desktop Launcher.

---
+++ desktop/src/com/teca/batalhanaval/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.teca.batalhanaval.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.teca.batalhanaval.BatalhaNaval;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BatalhaNaval(), config);
+	}
+}
